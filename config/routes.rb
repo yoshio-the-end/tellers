5757ca54f5de04ed8dfa126fb7ae84a8c7cb5789
yoshio-the-end <shoma.yoshikawa9126@gmail.com>
2019-11-13 20:55:34 +0900

    LINEとTwitterのシェア・いいねボタン追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'posts#index'
+
   namespace :posts do
     resources :searches, only: :index
   end
-  resources :posts, except: :index
-  resources :users, only: :show
+  resources :posts, except: :index do
+    resources :likes, only: [:create, :destroy]
+  end
+  resources :users, only: [:index, :show]
   resources :categories, only: [:index, :show]
 end
